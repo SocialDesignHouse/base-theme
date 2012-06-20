d9d20eb1657ba9e40a803f9af8e58da834092832
Eric Allen <eric@socialdesignhouse.com>
2012-06-20 16:08:46 -0400

    Updated functions.php to remove duplicate function.

--- themes/socialbase/functions.php
+++ themes/socialbase/functions.php
@@ -138,21 +138,6 @@ function nav_id_filter($id, $item) {
 add_filter('nav_menu_item_id','nav_id_filter',10,2 );
 
 ///////////////////////////////////
-// Show post thumbnails in RSS feeds
-///////////////////////////////////
-
-function featuredtoRSS($content) {
-	global $post;
-	if ( has_post_thumbnail( $post->ID ) ){
-		$content = '<div>' . get_the_post_thumbnail( $post->ID, 'full', array( 'style' => 'margin-bottom: 15px;' ) ) . '</div>' . $content;
-	}
-	return $content;
-}
-
-add_filter('the_excerpt_rss', 'featuredtoRSS');
-add_filter('the_content_feed', 'featuredtoRSS');
-
-///////////////////////////////////
 // Enqueue Register Script
 ///////////////////////////////////
 
