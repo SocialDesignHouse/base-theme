fd1bad6faf45fe547c20fd7b7c14a978ceca6332
Eric Allen <decoy@internetalche.me>
2012-09-19 19:14:46 -0400

    Updated formatting of footer.

--- themes/socialbase/footer.php
+++ themes/socialbase/footer.php
@@ -1,6 +1,9 @@
-	<footer id="footer">
-	</footer>
-<?php wp_footer(); ?>
-<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>	
-</body>
+		<footer id="footer">
+		</footer>
+		
+		<?php wp_footer(); ?>
+		
+		<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>	
+	
+	</body>
 </html>
